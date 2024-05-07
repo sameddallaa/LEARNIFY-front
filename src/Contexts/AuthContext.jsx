a2785dbbd506b23dd9cd39d3612c2198e01e0875
Samed Dallaa <samadodallaa@gmail.com>
2024-05-07 12:43:16 +0100

    test

--- src/Contexts/AuthContext.jsx
+++ src/Contexts/AuthContext.jsx
@@ -20,8 +20,8 @@ export const AuthProvider = ({ children }) => {
   const navigate = useNavigate();
 
   const login = async (email, password) => {
+    const endpoint = "http://localhost:8000/api/auth/login/";
     // const endpoint = "https://elearn-n48v.onrender.com/api/auth/login/";
-    const endpoint = "https://elearn-n48v.onrender.com/api/auth/login/";
     try {
       const response = await axios.post(
         endpoint,
@@ -59,8 +59,8 @@ export const AuthProvider = ({ children }) => {
   };
 
   const updateTokens = async () => {
-    // const endpoint = "http://localhost:8000/api/auth/login/refresh/";
-    const endpoint = "https://elearn-n48v.onrender.com/api/auth/login/";
+    const endpoint = "http://localhost:8000/api/auth/login/refresh/";
+    // const endpoint = "https://elearn-n48v.onrender.com/api/auth/login/";
     const response = await axios.post(
       endpoint,
       {
